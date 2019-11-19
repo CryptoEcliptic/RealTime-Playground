e8d6ab8f41cda3e55c21fca8007629567827fc55
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-11-19 18:24:39 +0200

    Added chicken group functionality

--- SignalR/SignalRDemo/SignalRDemo/wwwroot/js/chat.js
+++ SignalR/SignalRDemo/SignalRDemo/wwwroot/js/chat.js
@@ -7,13 +7,20 @@ let pElMinutes = document.createElement("p");
 let pElDays = document.createElement("p");
 let pElChickens = document.createElement("p");
 let pElChickensGroup = document.createElement("p");
-let index = 1;
+let index = 0;
 
 connection.on("ReceiveStatusUpdate", function (minutes, days, chickens, chickensGroup) {
-    pElMinutes.textContent = minutes + " " + index++;
+    pElMinutes.textContent = minutes + " " + index;
     pElDays.textContent = days;
     pElChickens.textContent = chickens;
     pElChickensGroup.textContent = chickensGroup;
+
+    if (index % 60 == 0) {
+        createChickens(chickensGroup);
+    }
+    index++;
+
+
 });
 
 parent.appendChild(pElDays);
@@ -26,4 +33,22 @@ connection.start().then(function () {
         return console.error(err.toString());
     });
 });
+function createChickens(groupSize) {
+    let size = parseInt(groupSize);
+    let parentEl = document.getElementById("chicken-container");
+    while (parentEl.firstChild) {
+        parentEl.removeChild(parentEl.firstChild);
+    }
+    for (let i = 0; i < size; i++) {
+        let imgChickenElem = document.createElement("img");
+        imgChickenElem.src = "/images/fchicken_smallf.jpg";
+        imgChickenElem.classList.add("w3-circle");
+        imgChickenElem.alt = "Chicken";
+       
+        parentEl.appendChild(imgChickenElem);
+    }
+   
+}
+
+
 
